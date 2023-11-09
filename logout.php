03910a1cea8f07ed7727f8e04a4b5a7b22fbe05d
annjoyceLoterte <annjoyceloterte@gmail.com>
2023-11-10 02:23:19 +0800

    deleting unimportant files

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); //start session
-session_destroy(); // distroy all the current sessions
-$url = 'home.php'; // redirect to login page
-header('Location: ' . $url); // redireted to login page
-
-?>
